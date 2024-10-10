6402a38a5599c8ac31d4d6233146f896905e67f5
abandoned-knowledge <gleb.evdokimov.1@gmail.com>
2024-10-10 18:16:11 +0300

    install all dependecies and configute the configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  ssr: false,
+  modules: ["@nuxtjs/tailwindcss"],
+  srcDir: "./src",
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: "modern",
+        },
+      },
+    },
+  },
+  imports: {
+    dirs: [],
+  },
+  tailwindcss: {
+    viewer: false,
+    cssPath: [
+      "~/app/style/tailwind.css",
+      {
+        injectPosition: "first",
+      },
+    ],
+  },
+});
